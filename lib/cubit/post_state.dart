42928ba816b17aea740056e11cc74b97a617159c
Nabeh119 <nabeh119y@gmail.com>
2026-03-30 03:52:03 +0300

    update project

--- lib/cubit/post_state.dart
+++ lib/cubit/post_state.dart
@@ -1,5 +1,9 @@
-class PostState {
-  PostState();
+import 'package:equatable/equatable.dart';
+
+class PostState extends Equatable {
+  @override
+  // TODO: implement props
+  List<Object?> get props => [];
 }
 
 class PostLoading extends PostState {}
@@ -7,9 +11,12 @@ class PostLoading extends PostState {}
 class PostLoaded extends PostState {
   final List<dynamic> posts;
   PostLoaded(this.posts);
+  List<Object?> get props => [posts];
 }
 
 class PostError extends PostState {
   final String message;
   PostError(this.message);
+  @override
+  List<Object?> get props => [message];
 }
